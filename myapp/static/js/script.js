67544897f343232526efd82b8f82b675bce8bd70
jerev7 <verdet.jeremie@gmail.com>
2020-07-21 19:26:58 +0200

    added some backend function

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -1,10 +1,23 @@
+// $(document).ready(function() {
+//     $("#submit_button").on( 'click', function (event) {
+//     	// event.preventDefault();
+//     	var $mynote = $("#note").val();
+//     	if ($mynote != "") {
+//     		event.preventDefault();
+//     		alert("Voilà votre question très intéressante :\n" + $mynote)
+//     	};
+//     });
+// });
 $(document).ready(function() {
-    $("#submit_button").on( 'click', function (event) {
-    	// event.preventDefault();
-    	var $mynote = $("#note").val();
-    	if ($mynote != "") {
-    		event.preventDefault();
-    		alert("Voilà votre question très intéressante :\n" + $mynote)
-    	};
+    $("#submit_button").on('click', function (event) {
+    	event.preventDefault();
+    	$.getJSON("/backend_process", {
+    		note: $("#note").val(),
+    	}, function(data) {
+    		if (data.backend_result != "") {
+    			event.preventDefault();
+    			alert(data.backend_result)
+    		};
+    	});
     });
 });
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,3 +7,12 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html',
 							logo_img=url_for('static', filename="images/robot.jpg"))
+
+# route to get data and work on them backend
+@app.route('/backend_process')
+def backend_process():
+	my_note = request.args.get('note', 0, type=str)
+	if my_note != "":
+		return jsonify(backend_result = 'votre question est : ' + my_note)
+	else:
+		return jsonify(backend_result="vous n'avez rien écrit dans la case imbécile")

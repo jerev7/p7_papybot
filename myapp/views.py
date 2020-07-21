ebf6c9fbdb7c0e2099ed133d4e3f3c49903fb05e
jerev7 <verdet.jeremie@gmail.com>
2020-07-21 11:09:39 +0200

    added js script

--- myapp/views.py
+++ myapp/views.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index/')
 def index():
-	return render_template('index.html', logo_img=url_for('static', filename="images/robot.jpg"))
+	return render_template('index.html',
+							logo_img=url_for('static', filename="images/robot.jpg",
+							script_js=url_for('static', filename="js/script.js")))

085238951044c4feed92a136c9c9427f89257e05
jerev7 <verdet.jeremie@gmail.com>
2020-07-22 19:12:05 +0200

    improved jquery script, fixed favicon issue and img f from google map loading 2 times

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -8,18 +8,48 @@
 //     	};
 //     });
 // });
+
+function updateQuestion(route) {
+	$.getJSON(route, {
+	    		note: $("#note").val(),
+	    	}, function(data) { 
+	    			event.preventDefault();
+	    			$("#imggoogle").attr('src', data.backend_result);
+	    			$("#imggoogle").show();
+	    	});
+}
+$("#imggoogle").hide();
 $(document).ready(function() {
-	$("#imggoogle").hide();
+	$("#result").hide();
     $("#submit_button").on('click', function (event) {
-    	event.preventDefault();
-    	$.getJSON("/backend_process", {
-    		note: $("#note").val(),
-    	}, function(data) {
-    		if (data.backend_result != "") {
-    			event.preventDefault();
-    			$("#imggoogle").attr('src', data.backend_result);
-    			$("#imggoogle").show();
-    		} else {$("#imggoogle").hide()};
-    	});
+    	if ($("#note").val() != "") {
+    		event.preventDefault();
+    		$("#result").text("Votre question est : " + $("#note").val());
+    		$("#result").show();
+	    	updateQuestion("/backend_process");
+	    };
     });
 });
+
+
+
+  
+// function dowiki(place) {
+//     var URL = 'https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=';
+
+//     URL += "&titles=" + place;
+//     URL += "&rvprop=content";
+//     URL += "&callback=?";
+//     $.getJSON(URL, function (data) {
+//         var obj = data.query.pages;
+//         var ob = Object.keys(obj)[0];
+//         try{
+//             document.getElementById('result').textContent = obj[ob]["extract"];
+//         }
+//         catch (err) {
+//             document.getElementById('result').textContent = err.message;
+//         }
+
+//     });
+// }
+// dowiki("Openclassrooms")

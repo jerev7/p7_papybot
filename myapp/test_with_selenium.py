afd0cfbeccfb97a3c59bd1a8dab2d5cb8540b488
jerev7 <verdet.jeremie@gmail.com>
2020-08-10 16:11:32 +0200

    added some test with selenium

--- myapp/test_with_selenium.py
+++ myapp/test_with_selenium.py
@@ -1,5 +1,36 @@
+import time
 import myapp.views
 import myapp.config
 from flask_testing import LiveServerTestCase
 from selenium import webdriver
+import selenium.webdriver.support.ui as ui
 
+class TestUserInterface(LiveServerTestCase):
+
+	def create_app(self):
+		return myapp.views.app
+
+	def setUp(self):
+		self.driver = webdriver.Firefox()
+		self.driver.get("http://127.0.0.1:5000")
+		self.wait = ui.WebDriverWait(self.driver, 1000)
+
+	def tearDown(self):
+		self.driver.quit()
+		
+
+	def test_input_stuff(self):
+		# self.wait.until(lambda driver: self.driver.find_element_by_css_selector("#note").is_displayed())
+		new_input = self.driver.find_element_by_css_selector("#note")
+		new_input.send_keys("Openclassrooms")
+		assert new_input.get_attribute("value") == "Openclassrooms"
+		button = self.driver.find_element_by_css_selector("#submit_button")
+		button.click()
+		time.sleep(6)
+		assert self.driver.find_element_by_css_selector("#imggoogle").is_displayed()
+		
+
+	# def test_user_interacts():
+	# 	self.create_server()
+	# 	self.input_stuff()
+		# assert self.driver.find_element_by_name("note").value == "Openclassrooms"
